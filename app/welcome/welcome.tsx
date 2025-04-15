59c845a4c1c4db7f07ad8f18476793dc9afd1a89
hema <hema@m5nv.com>
2025-04-15 15:51:43 -0700

    initial commit

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,6 +1,8 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
 
+import { Button } from '@/components/ui/button'
+
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
@@ -40,6 +42,9 @@ export function Welcome() {
               ))}
             </ul>
           </nav>
+          <div>
+            <Button>Shadcn</Button>
+          </div>
         </div>
       </div>
     </main>
